d446daed0240f76b4960f31ea53f857c7a28e4c2
José Danúbio <danubioalves@gmail.com>
2024-04-20 11:22:19 -0300

    bottomnav

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _paginaAtual = 0;
+  final List<Widget> _paginas = [
+    const Text('Página 1'),
+    const Text('Página 2'),
+    const Text('Página 3'),
+    const Text('Página 4'),
+    const Text('Página 5'),
+  ];
+
+  void _mudarPagina(int index) {
+    setState(() {
+      _paginaAtual = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: _paginas[_paginaAtual]),
+        bottomNavigationBar: BottomNavigationBar(
+          fixedColor: Colors.deepPurple,
+          unselectedItemColor: Colors.red,
+          currentIndex: _paginaAtual,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Receitas',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Meal of the day',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Loja',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: 'Minhas Receitas',
+            ),
+          ],
+          onTap: _mudarPagina,
+        ),
+      ),
+    );
   }
 }
